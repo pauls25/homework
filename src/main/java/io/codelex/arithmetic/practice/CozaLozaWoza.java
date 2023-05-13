8b27e1443bee3c9052abb93f97eb98f478dc9eef
pauls25 <76559243+pauls25@users.noreply.github.com>
2023-05-13 14:39:48 +0300

    Correct CozaLozaWoza loops

--- src/main/java/io/codelex/arithmetic/practice/CozaLozaWoza.java
+++ src/main/java/io/codelex/arithmetic/practice/CozaLozaWoza.java
@@ -3,26 +3,23 @@ package io.codelex.arithmetic.practice;
 public class CozaLozaWoza {
     public static void main(String[] args) {
 
-        int i = 1;
-        while (i < 111) {
-            for (int l = 0; l < 10; l++) {
-                for (int j = 0; j < 11; j++) {
-                    if (i % 3 == 0 && i % 5 == 0) {
-                        System.out.print("CozaLoza ");
-                    } else if (i % 5 == 0 && i % 7 == 0) {
-                        System.out.print("LozaWoza ");
-                    } else if (i % 3 == 0) {
-                        System.out.print("Coza ");
-                    } else if (i % 5 == 0) {
-                        System.out.print("Loza ");
-                    } else if (i % 7 == 0) {
-                        System.out.print("Woza ");
-                    } else {
-                        System.out.print(i + " ");
-                    }
-                    i += 1;
-
-                }
+        for (int i = 0; i <= 110; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("CozaLoza ");
+            } else if (i % 5 == 0 && i % 7 == 0) {
+                System.out.print("LozaWoza ");
+            } else if (i % 3 == 0 && i % 7 == 0) {
+                System.out.print("CozaWoza ");
+            } else if (i % 3 == 0) {
+                System.out.print("Coza ");
+            } else if (i % 5 == 0) {
+                System.out.print("Loza ");
+            } else if (i % 7 == 0) {
+                System.out.print("Woza ");
+            } else {
+                System.out.print(i + " ");
+            }
+            if (i % 11 == 0) {
                 System.out.print("\n");
             }
         }
