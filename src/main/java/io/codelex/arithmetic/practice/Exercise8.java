f4285fd21da630438fce113d70b2307e53dec03f
pauls25 <76559243+pauls25@users.noreply.github.com>
2023-05-13 14:27:20 +0300

    Refactor Exercise8 to cammelCase

--- src/main/java/io/codelex/arithmetic/practice/Exercise8.java
+++ src/main/java/io/codelex/arithmetic/practice/Exercise8.java
@@ -3,12 +3,12 @@ package io.codelex.arithmetic.practice;
 public class Exercise8 {
     public static void main(String[] args) {
 
-        calculate_total_pay(7.50, 35);
-        calculate_total_pay(8.20, 47);
-        calculate_total_pay(10.00, 73);
+        calculateTotalPay(7.50, 35);
+        calculateTotalPay(8.20, 47);
+        calculateTotalPay(10.00, 73);
     }
 
-    public static void calculate_total_pay(double basePay, int hoursWorked) {
+    public static void calculateTotalPay(double basePay, int hoursWorked) {
         double salary;
 
         int overTimeHours = hoursWorked - 40;
